dde583c336f8c055b7c3b5acbe51a6a24bad5d07
hwang822 <hwang2k@gmail.com>
2018-07-19 13:29:40 -0400

    update

--- MixwellSoftware/WixwellWPF/Views/MainWindow.xaml.cs
+++ MixwellSoftware/WixwellWPF/Views/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MixwellWPF.ViewModels;
+using MixwellWPF.Models;
 
 namespace MixwellWPF.Views
 {
