8ab2d48ddf55ded9ca3b9a6449239441e991d77f
hwang822 <hwang2k@gmail.com>
2018-08-14 18:13:15 -0400

    update

--- MixwellSoftware/MixwellAngular/src/app/mock-services.ts
+++
@@ -1,8 +0,0 @@
-import { Service } from './service';
-
-export const SERVICES: Service[] = [
-  { id: 11, name: 'Engineering' },
-  { id: 12, name: 'Program Management' },
-  { id: 13, name: 'Buessiness Management' },
-  { id: 14, name: 'Information Technology' }
-];
